cd05e052366b50ea8658fbe2cc55dfcee7c3cb59
mathieupelletier-db <mathieupelletier-db>
2025-04-14 15:20:43 +0000

    WIP

--- src/spatial/points/las_reader.py
+++ src/spatial/points/las_reader.py
@@ -15,44 +15,27 @@ except ImportError as e:
 
 class LASToGeometryDataSourceReader(DataSourceReader):
     """
-    Data source reader to read LAS/LAZ files and convert them to dataframe.
-
-    This class processes LAS/LAZ files to extract geometries (e.g., points, lines, or polygons)
-    and associated metadata (tags). The output can be configured to include geometries in
-    Well-Known Text (WKT), Well-Known Binary (WKB), or GeoJSON formats.
+    Data source reader to read LAS/LAZ files and convert them to a dataframe.
 
     Attributes:
         schema (StructType): The schema of the output data, including fields for x, y, z, intensity and headers.
         options (dict): Configuration options to customize the data reader.
 
     Options:
-        - 1,2,3
-        - chunk size (or not)
-        - point format 0..9
-        - `path` (str): The file path to the input PBF file. **Required**.
-        - `geometryType` (str): The output geometry format. Supported values:
-          - `"WKT"`: Well-Known Text (default).
-          - `"WKB"`: Well-Known Binary.
-          - `"GeoJSON"`: GeoJSON format.
-        - `emptyTagFilter` (bool): Whether to exclude OSM elements with no tags. Default: `True`.
-        - `keyFilter` (str): A key-based filter for OSM tags. Only elements with this key will be processed. Example: `"highway"`.
-        - `tagFilter` (str): A filter based on specific key-value tag pairs. Should be a tuple-like string, e.g., `"('amenity', 'cafe')"`.
-          Only elements with this key-value pair will be processed.
+        - `path` (str): The file path to the input LAS/LAZ file. **Required**.
+        - `chunkSize` (int): Process records in chunks of `chunkSize` records at a time. Defaults to 1000000.
 
     Example Usage:
         df = (
             spark.read.format("las")
             .option("path", path)
-            .option("geometryType", "WKT")
-            .option("emptyTagFilter", True)
-            .option("keyFilter", "building")
-            .option("tagFilter", "('building', 'hospital')")
+            .option("chunkSize", "1000000")
             .load()
         )
     """
     def __init__(self, schema: StructType, options: dict):
         """
-        Initialize the PBFToGeometryDataSourceReader.
+        Initialize the LASToGeometryDataSourceReader.
 
         Args:
             schema (StructType): The schema of the output data.
@@ -61,39 +44,59 @@ class LASToGeometryDataSourceReader(DataSourceReader):
         self.schema: StructType = schema
         self.options: dict = options
 
+    def check_directory(directory_path):
+        if os.path.isdir(directory_path):
+            print(f"Directory exists: {directory_path}")
+        else:
+            print(f"Directory does not exist: {directory_path}")
+
     def read(self, partition: Optional[int] = None) -> Generator[Tuple[int, str, Optional[str], Dict[str, str]], None, None]:
         """
-        Read the PBF file and yield geometry data for each OSM element.
+        Read the LAS file and yield data for each points.
 
         Args:
             partition (Optional[int]): Partition index, if applicable. Not implemented.
 
         Yields:
-            tuple: A tuple containing the element ID, type, geometry, and tags.
+            tuple: A tuple containing the point's data
         """
         # Extract options
         input_path: str = self.options.get("path")
         if not input_path:
             raise ValueError("The 'path' option is required.")
 
-        # Handle file vs directory
-        #las = laspy.read(input_path)
-#        for filename in os.listdir(input_dir):
- #   if filename.endswith('.las'):
-  #      input_path = os.path.join(input_dir, filename)
+        chunk_size: int = self.options.get("chunkSize", 1000000)
+
+        # TODO: process files in a directory (for now supports only file)
 
         with laspy.open(input_path) as f:
-            tags: Dict[str, str] = {}
-            chunk_size = 1000000  # Adjusted chunk size
 
-            #points_list = []  # Accumulate points in batches
+            # TODO: perform scaling here instead of using already scaled values
+
             for points in f.chunk_iterator(chunk_size):
                 x_float = np.array(points.x).astype(float)
                 y_float = np.array(points.y).astype(float)
                 z_float = np.array(points.z).astype(float)
                 
-                for x, y, z, intensity in zip(x_float, y_float, z_float, points.intensity):
-                    yield (x, y, z, intensity, tags)
+                # if gps_time not available yield null
+
+                # Check if the 'green' field is present in the 'las' object
+                if hasattr(las, 'gps_time') and hasattr(las, 'red'):
+                    for point in zip(x_float, y_float, z_float, points.intensity, points.return_number, points.number_of_returns, points.scan_direction_flag, points.edge_of_flight_line,   points.classification, points.synthetic, points.key_point, points.withheld, points.scan_angle_rank, points.user_data, points.point_source_id, points.gps_time, points.red, points.green, points.blue):
+                        yield point
+
+                elif hasattr(las, 'gps_time'):
+                    for point in zip(x_float, y_float, z_float, points.intensity, points.return_number, points.number_of_returns, points.scan_direction_flag, points.edge_of_flight_line,   points.classification, points.synthetic, points.key_point, points.withheld, points.scan_angle_rank, points.user_data, points.point_source_id, points.gps_time):
+                        yield point
+
+                else:
+                    for point in zip(x_float, y_float, z_float, points.intensity, points.return_number, points.number_of_returns, points.scan_direction_flag, points.edge_of_flight_line,   points.classification, points.synthetic, points.key_point, points.withheld, points.scan_angle_rank, points.user_data, points.point_source_id):
+                        yield point
+
+                #for x, y, z, intensity in zip(x_float, y_float, z_float, points.intensity, points.return_number, points.number_of_returns, points.scan_direction_flag, points.#edge_of_flight_line, points.classification, points.synthetic, points.key_point, points.withheld, points.scan_angle_rank, points.user_data, points.point_source_id, points.#gps_time, points.red, points.green, points.blue):
+                #    yield (x, y, z, intensity, return_number, number_of_returns, scan_direction_flag, edge_of_flight_line, classification, synthetic, key_point, withheld, #scan_angle_rank, user_data, point_source_id, gps_time, red, green, blue)
+
+                # R,G,B not available yield null
 
 class LASToGeometryDataSource(DataSource):
     """
@@ -113,44 +116,61 @@ class LASToGeometryDataSource(DataSource):
     def schema(self) -> StructType:
         """
         Define the schema for the output data.
+        The schema includes fields present in point format 3.
 
         Returns:
-            StructType: The schema including fields for ID, type, geometry, and tags.
-            ['X',
- 'Y',
- 'Z',
- 'intensity',
- 'return_number',
- 'number_of_returns',
- 'scan_direction_flag',
- 'edge_of_flight_line',
- 'classification',
- 'synthetic',
- 'key_point',
- 'withheld',
- 'scan_angle_rank',
- 'user_data',
- 'point_source_id',
- 'gps_time']
-point format 0
+            StructType: The schema including these fields:
+            - x: scaled x coordinate
+            - y: scaled y coordinate
+            - z: scaled z coordinate 
+            - intensity: the integer representation of the pulse return magnitude
+            - return_number: is the pulse return number for a given output pulse
+            - number_of_returns: total number of returns for a given pulse
+            - scan_direction_flag: the direction at which the scanner mirror was traveling at the time of the output pulse
+            - edge_of_flight_line: data bit has a value of 1 only when the point is at the end of a scan. It is the last point on a given scan line before it changes direction.
+            - classification: point classes
+            - synthetic:
+            - key_point:
+            - withheld:
+            - scan_angle_rank: valid range from -90 to +90. The Scan Angle Rank is the angle (rounded to the nearest integer in the absolute value sense) at which the laser point was output from the laser system including the roll of the aircraft.  
+            - user_data: additional information about the point
+            - point_source_id: This value indicates the file from which this point originated.
+            - gps_time:  double floating point time tag value at which the point was acquired
+            - red: The Red image channel value associated with this point 
+            - green: The Green image channel value associated with this point 
+            - blue: The Blue image channel value associated with this point 
         """
         return StructType([
             StructField("x", FloatType(), True),
             StructField("y", FloatType(), True),
             StructField("z", FloatType(), True),
             StructField("intensity", ShortType(), True),
-            StructField("tags", MapType(StringType(), StringType()), True)
+            StructField("return_number", ShortType(), True),
+            StructField("number_of_returns", ShortType(), True),
+            StructField("scan_direction_flag", ByteType(), True),
+            StructField("edge_of_flight_line", ByteType(), True),
+            StructField("classification", ByteType(), True),
+            StructField("synthetic", ByteType(), True),
+            StructField("key_point", ByteType(), True),
+            StructField("withheld", ByteType(), True),
+            StructField("scan_angle_rank", ByteType(), True),
+            StructField("user_data", ByteType(), True),
+            StructField("point_source_id", ShortType(), True),
+            StructField("gps_time", DoubleType(), True),
+            StructField("red", ShortType(), True),
+            StructField("green", ShortType(), True),
+            StructField("blue", ShortType(), True)
         ])
 
     def reader(self, schema: StructType) -> LASToGeometryDataSourceReader:
         """
-        Create a data source reader for reading the PBF file.
+        Create a data source reader for reading the LAS file.
 
         Args:
             schema (StructType): The schema of the output data.
 
         Returns:
-            PBFToGeometryDataSourceReader: An instance of the data source reader.
+            LASToGeometryDataSourceReader: An instance of the data source reader.
         """
         return LASToGeometryDataSourceReader(schema, self.options)
 
